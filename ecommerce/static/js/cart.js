d1c6bf555105dd3db04d3e0cf4028e75e4e6d0e5
aashishg07 <ash.gtm07@gmail.com>
2022-11-28 16:50:45 +0545

    added new feature

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,8 @@
+var updateButton = document.getElementsByClassName('update-cart')
+for(var i=0; i<updateButton.length; i++){
+  updateButton[i].addEventListener('click', function(){
+    var productId = this.dataset.product
+    var action = this.dataset.action
+    console.log('productId:', productId, 'action:', action)
+  })
+}
