86ec57fab31bd26d5f36714f36b0a943ad4d2473
aashishg07 <ash.gtm07@gmail.com>
2022-11-30 22:12:56 +0545

    updated ecommerce

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -4,5 +4,36 @@ for(var i=0; i<updateButton.length; i++){
     var productId = this.dataset.product
     var action = this.dataset.action
     console.log('productId:', productId, 'action:', action)
+
+    console.log('USER:', user)
+
+    if(user === 'AnonymousUser'){
+      console.log('User is not logged in.')
+    }
+    else
+    {
+      UpdateUserOrder(productId, action)
+    }
+  })
+}
+
+
+function UpdateUserOrder(productId, action){
+  var url = '/update_item/'
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': csrftoken
+    },
+    body: JSON.stringify({'productId': productId, 'action': action})
+  })
+
+  .then((response)=>{
+    return response.json()
+  })
+  .then((data)=>{
+    console.log('data:', data)
+    location.reload()
   })
 }
